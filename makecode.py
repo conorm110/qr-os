ad72e1d37072369ccd99ffca473dc8351009b65c
conorm <mjoconnor110@gmail.com>
2021-05-18 19:52:41 -0500

    finished qr bit

--- makecode.py
+++ makecode.py
@@ -3,18 +3,12 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 import cairosvg
-
 filename = 'bin/out.7z'
 with open(filename, 'rb') as f:
     content = f.read()
 s = str(binascii.hexlify(content))
 print(s)
-
-# Generate QR code
 url = pyqrcode.create(s)
-  
-# Create and save the svg file naming "myqr.svg"
-url.svg("os.svg", scale = 8)
-
-cairosvg.svg2pdf(url='os.svg', write_to='os.pdf')
-cairosvg.svg2png(url='os.svg', write_to='os.png')
+url.svg("out/os.svg", scale = 8)
+cairosvg.svg2pdf(url='out/os.svg', write_to='out/os.pdf')
+cairosvg.svg2png(url='out/os.svg', write_to='out/os.png')
